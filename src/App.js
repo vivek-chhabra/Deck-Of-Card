117528bc53b9a6df6427ea13fee4cc34c97b6a72
vivek chhabra <vivekcb8642@gmail.com>
2023-06-01 17:13:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GenerateCard from './GenerateCard';
+
+function App() {
+  return (
+    <div className="App flex">
+      <GenerateCard />
+    </div>
+  );
+}
+
+export default App;
